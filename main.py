d634b58b9bc9965238614d05f127c8cad6b6c02d
KahyanKingstech <kah.yan@kingstech.com.sg>
2025-02-27 17:15:33 +0800

    1

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from customer import router as customer_router
 from collection import router as collection_router
 from product import router as product_router
 from order import router as order_router
-from inventory import router as inventory_router
 
 app = FastAPI()
 
@@ -21,4 +20,3 @@ app.include_router(customer_router)
 app.include_router(collection_router)
 app.include_router(product_router)
 app.include_router(order_router)
-app.include_router(inventory_router)
